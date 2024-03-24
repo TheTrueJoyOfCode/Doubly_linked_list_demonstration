9e6355d7502f325abbdecd1cf428a414927717a0
TheTrueJoyOfCode <105929367+TheTrueJoyOfCode@users.noreply.github.com>
2024-03-24 20:45:22 +0000

    Made diagnostic messages more readable fixed formatting

--- main.cpp
+++ main.cpp
@@ -1,38 +1,43 @@
-#include <iostream>
 #include "LinkedList.cpp"
-
+#include <iostream>
 
 int main() {
   // test code for linked lists
   LinkedList *list = new LinkedList();
   //  edge case: empty list
-  std::cout << list->toString() << ":empty list" << std::endl;
+  std::cout << "Empty list: " << list->toString() << std::endl;
   //  removing from an empty list
-  list->remove(1);
-  std::cout << list->toString() << ":empty list" << std::endl;
+  std::cout << "Removing from an empty list: " << list->remove(1)
+            << " expeceted: -1" << std::endl;
   //  adding to an empty list
   list->add(1);
-  std::cout << list->toString() << ":1" << std::endl;
+  std::cout << "Empty list: " << list->toString() << std::endl;
   //  removing the only element
   list->remove(1);
-  std::cout << list->toString() << ":empty list" << std::endl;
+  std::cout << "Removing the only element: " << list->toString()
+            << "expeceted:" << std::endl;
   //  adding to a list
   list->add(1);
   list->add(2);
   list->add(3);
   list->add(4);
   list->add(5);
-  std::cout << list->toString() << ":1, 2, 3, 4, 5" << std::endl;
+  std::cout << "added to a list: " << list->toString()
+            << "expeceted: 1, 2, 3, 4, 5" << std::endl;
   // removing an elemt
   list->remove(3);
-  std::cout << list->toString() << ":1, 2, 4, 5" << std::endl;
+  std::cout << "removed an element: " << list->toString()
+            << "expeceted: 1, 2, 4, 5" << std::endl;
   // removing the last element
   list->remove(5);
-  std::cout << list->toString() << ":1, 2, 4" << std::endl;
+  std::cout << "removed the last element: " << list->toString()
+            << "expeceted: 1, 2, 4" << std::endl;
   // removing the first element
   list->remove(1);
-  std::cout << list->toString() << ":2, 4" << std::endl;
+  std::cout << "removed the first element: " << list->toString()
+            << "expeceted: 2, 4" << std::endl;
   // removing  element that is not present
   list->remove(1);
-  std::cout << list->toString() << ":2, 4" << std::endl;
+  std::cout << "removed an element tha is not present: " << list->toString()
+            << "expeceted: 2, 4" << std::endl;
 }
